e96669753a723784413068353728982a1db38699
StefanoChinatto <10550186@cooltura.com.br>
2026-07-23 11:26:06 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main() {
+
+    float notas [3]= {85.5, 90.0, 78.3}; // array de notas
+
+    printf ("notas do aluno 1 é: %.1f \n" ,notas [0]);
+    printf ("notas do alino 2 é: %.1f \n" ,notas [1]);
+    printf ("notas dos aluno 3 é: %.1f \n" ,notas [2]);
+
+    return 0;
+}
